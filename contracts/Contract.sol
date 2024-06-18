ca025a1bd80aad29762fd12084a527c9f56d1648
Franklin <franklin.op@hotmail.com>
2024-06-18 23:57:25 +0200

    adding different function set ups

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -1,6 +1,31 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract MyContract {
-    constructor() {}
+contract RealEstate {
+   
+
+
+   //FUNCTION IN CONTRACT
+
+   function listProperty() external returns (unit256) {
+   }
+
+    function updateProperty() external returns (unit256){}
+
+   function buyProperty() external payable{}
+
+     function getAllProperties() public view returns (Property[] memory){}
+
+     function getProperty() external view returns(){}
+
+     function getUserProperties() external view returns (Property[] memory){};
+    
+
+    //REVIEWS FUNCTION
+    function addReview() external{}
+
+    function getProductReviews() external view returns(Review[] memory){}
+
+    function getUser
+
 }
